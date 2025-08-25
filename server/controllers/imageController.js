189a4949450bfadefc5b00a368919359c59b947f
Elena Cosma <elenacosma2004@gmail.com>
2025-08-25 23:16:53 +0300

    backend

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import userModel from "../models/userModel";
+import FormData from "form-data";
 
 export const generateImage = async (req, res) => {
   try {
@@ -17,6 +19,23 @@ export const generateImage = async (req, res) => {
         creditBalance: user.creditBalance,
       });
     }
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: {
+          "x-api-key": process.env.CLIPDROP_API,
+        },
+        responseType: "arraybuffer",
+      }
+    );
+
+    const base64Image = Buffer.from(data, "binary").toString("base64");
+
+    const resultImage = `data:image/png;base64,${base64Image}`;
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
